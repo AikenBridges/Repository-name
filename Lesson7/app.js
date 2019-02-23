9a9e51bc7cb6f4ccc0baa1610c5b11a7413c8731
AikenBridges <psychicsandshrew@gmail.com>
2019-02-23 16:12:00 +0800

    lesson7

--- Lesson7/app.js
+++ Lesson7/app.js
@@ -28,8 +28,13 @@ function create(){
 	var style = {font: 'bold 32px Arial', fill:'#fff'}
 	scorelabel = game.add.text(300,560,'score: ', style)
 	scoretext = game.add.text(420,560,score, style)
+	scorelabel.setShadow(3,3,"rgba(0,0,0,0.5",2)
+	scoretext.setShadow(3,3,"rgba(0,0,0,0.5",2)
+
 	lifelabel = game.add.text(10,5,'lives: ', style)
 	lifetext = game.add.text(120,5,life, style)
+	lifelabel.setShadow(3,3,"rgba(0,0,0,0.5",2)
+	lifetext.setShadow(3,3,"rgba(0,0,0,0.5",2)
 }
 function update(){
 
