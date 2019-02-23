46c27dfaf7940a44aa4675757f1ceecf8aa443df
AikenBridges <psychicsandshrew@gmail.com>
2019-02-23 15:49:26 +0800

    lesson7

--- Lesson7/app.js
+++ Lesson7/app.js
@@ -4,31 +4,31 @@ var score = 0;
 var life = 3;
 
 function preload(){
-	game.load.image ("sky","assets/sky.png");
-	game.load.image ("platform","assets/platform.png");
-	game.load.image ("star","assets/star.png");
-	game.load.spritesheet ("player","assets/dude.png",32,48);
-	game.load.spritesheet ("baddie","assets/baddie.png",32,32);
+	game.load.image ('sky','assets/sky.png');
+	game.load.image ('platform','assets/platform.png');
+	game.load.image ('star','assets/star.png');
+	game.load.spritesheet ('dude','assets/dude.png',32,48);
+	game.load.spritesheet ('baddie','assets/baddie.png',32,32);
 }
 function create(){
 	game.physics.startSystem(Phaser.Physics.ARCADE);
 	
-	game.add.sprite(0,0,"sky");
+	game.add.sprite(0,0,'sky');
 
 	platforms = game.add.physicsGroup();
 	platforms.enableBody = true;
 
-	var ground = platforms.create(0, 550, "platform");
+	var ground = platforms.create(0, 550, 'platform');
 	ground.scale.setTo(2,2);
-	var ledge = platforms.create(400, 400, "platform");
+	var ledge = platforms.create(400, 400, 'platform');
 	ledge.body.immovable = true;
-	ledge = platforms.create(-100, 250, "platform");
+	ledge = platforms.create(-100, 250, 'platform');
 	ledge.body.immovable = true;
 
-	var style = {font: "bold 32px Arial", fill:"#fff"}
-	scorelabel = game.add.text(300,560,"score: ", style)
+	var style = {font: 'bold 32px Arial', fill:'#fff'}
+	scorelabel = game.add.text(300,560,'score: ', style)
 	scoretext = game.add.text(300,560,score, style)
-	lifelabel = game.add.text(10,5,"lives: ", style)
+	lifelabel = game.add.text(10,5,'lives: ', style)
 	lifetext = game.add.text(120,5,life, style)
 }
 function update(){
